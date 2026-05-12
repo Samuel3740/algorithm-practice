aced372d300856766fdde32e294b0b0e0866fb78
Samuel <roysangjun@gmail.com>
2026-05-12 23:17:33 +0900

    [level 0] Title: 배열 만들기 4, Time: 0.95 ms, Memory: 6.45 MB -BaekjoonHub

--- 프로그래머스/0/181918. 배열 만들기 4/배열 만들기 4.cpp
+++ 프로그래머스/0/181918. 배열 만들기 4/배열 만들기 4.cpp
@@ -3,8 +3,8 @@
 using namespace std;
 
 vector<int> solution(vector<int> arr) {
-    vector<int> stk;
     int i = 0;
+    vector<int> stk;
     
     while (i < arr.size()) {
         if (stk.empty()) {
