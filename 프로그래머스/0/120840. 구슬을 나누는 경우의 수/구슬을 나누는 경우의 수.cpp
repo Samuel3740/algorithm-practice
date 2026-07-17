359cbe61f877c73cc04c17f6b79a42d14e2819be
Samuel <roysangjun@gmail.com>
2026-07-17 19:10:26 +0900

    [level 0] Title: 구슬을 나누는 경우의 수, Time: 0.01 ms, Memory: 5.01 MB -BaekjoonHub

---
+++ 프로그래머스/0/120840. 구슬을 나누는 경우의 수/구슬을 나누는 경우의 수.cpp
@@ -0,0 +1,16 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int solution(int balls, int share) {
+    long long answer = 1;
+    
+    share = min(share, balls - share);
+
+    for (int i = 1; i <= share; ++i) {
+        answer = answer * (balls - i + 1) / i;
+    }
+    
+    return answer;
+}
