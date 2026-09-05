19628c1e3494af77b51e79b1dbd5e3f2cd114879
Samuel <roysangjun@gmail.com>
2026-09-05 16:22:13 +0900

    [level 0] Title: 정수를 나선형으로 배치하기, Time: 0.16 ms, Memory: 4.89 MB -BaekjoonHub

---
+++ 프로그래머스/0/181832. 정수를 나선형으로 배치하기/정수를 나선형으로 배치하기.cpp
@@ -0,0 +1,36 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+vector<vector<int>> solution(int n) {
+vector<vector<int>> answer(n, vector<int>(n, 0));
+
+    int dr[] = {0, 1, 0, -1};
+    int dc[] = {1, 0, -1, 0};
+
+    int r = 0;
+    int c = 0;
+    int dir = 0;
+
+    for (int num = 1; num <= n * n; num++) {
+        answer[r][c] = num;
+
+        int nr = r + dr[dir];
+        int nc = c + dc[dir];
+
+        if (nr < 0 || nr >= n ||
+            nc < 0 || nc >= n ||
+            answer[nr][nc] != 0) {
+            
+            dir = (dir + 1) % 4;
+            nr = r + dr[dir];
+            nc = c + dc[dir];
+        }
+
+        r = nr;
+        c = nc;
+    }
+    
+    return answer;
+}
