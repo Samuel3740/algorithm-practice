c4d704f40376db1780cb14b406fee13609706a19
Samuel <roysangjun@gmail.com>
2026-09-24 19:48:09 +0900

    [level 0] Title: OX퀴즈, Time: 0.02 ms, Memory: 4.79 MB -BaekjoonHub

---
+++ 프로그래머스/0/120907. OX퀴즈/OX퀴즈.cpp
@@ -0,0 +1,27 @@
+#include <string>
+#include <vector>
+#include <sstream>
+using namespace std;
+
+vector<string> solution(vector<string> quiz) {
+    vector<string> answer;
+    
+    for (const string& q : quiz) {
+        int X, Y, Z;
+        string op, eq;
+        stringstream ss(q);
+        
+        ss >> X >> op >> Y >> eq >> Z;
+        
+        int result = (op == "+") ? (X + Y) : (X - Y);
+        
+        if (result == Z) {
+            answer.push_back("O");
+        } else {
+            answer.push_back("X");
+        }        
+    }
+    
+    
+    return answer;
+}
